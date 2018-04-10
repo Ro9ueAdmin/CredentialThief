0d5ff5a189ec6a157644a214e86a8fb194f0fe06
matamorphosis <mbrittain15@gmail.com>
2018-04-10 12:26:17 +1000

    Delete keypresstestwithquotefquote.php

--- var/www/ardiuno car/keypresstestwithquotefquote.php
+++
@@ -1,6 +0,0 @@
-<?php
-// $q = $_REQUEST["q"]; // get the q parameter from URL
-$fp = fopen("/dev/ttyUSB0", "r+");
-fwrite($fp, "f"); // Output "f" parameter to ttyUSB0 serial port
-fclose($fp);
-?>
